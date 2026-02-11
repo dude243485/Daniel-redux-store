19b64a35fd45d323a49096984800f06845d9e8aa
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-02-11 12:35:23 +0100

    completed basic redux, just remains making asynchronous requests

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,8 +1,9 @@
 import CartItem from "./CartItem";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { openModal } from "../features/modal/modalSlice";
 
 const CartContainer = () => {
-
+    const dispatch = useDispatch()
     const {cartItems, total , amount} = useSelector((store) => store.cart)
     if (amount < 1){
         return <section className=" cart">
@@ -26,10 +27,12 @@ const CartContainer = () => {
                 <hr />
                 <div className = "cart-total">
                     <h4>
-                        total <span>$ {total}</span>
+                        total <span>$ {total.toFixed(2)}</span>
                     </h4>
                 </div>
-                <button className = "btn clear-btn">
+                <button 
+                onClick = { () => dispatch(openModal())}
+                className = "btn clear-btn">
                     Clear cart
                 </button>
             </footer>
